c9e8eb0da9370e5c40c704698ae102cd8424fd8b
Sandor Biba <sandorbiba@gmail.com>
2020-11-08 11:17:39 +0100

    school management project

---
+++ src/lesson/lesson.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
+
+@Module({
+  providers: [LessonResolver, LessonService],
+})
+export class LessonModule {}
